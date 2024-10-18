8ad3a10b1339d500edb866bee2b3081c2655c253
Felix Delgadillo <felix.d.barrios@gmail.com>
2024-10-18 17:57:26 -0600

    Se empieza con las rutas dinamicas

--- react-udemy/src/Components/PadreRutas.jsx
+++ react-udemy/src/Components/PadreRutas.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Componente1 from './Rutas/Componente1'
 import Componente2 from './Rutas/Componente2'
 import Componente3 from './Rutas/Componente3'
-import {BrowserRouter, Route,Link,Routes, NavLink} from 'react-router-dom'
+import RutasDinamicas from './RutasDinamicas'
+import {BrowserRouter, Route,Routes, NavLink} from 'react-router-dom'
 // Tambin existe Switch dentro de react-router-dom para evitar usar exact pero solo para eso aparentemente
+// Tambin esta el Link pero no deja poner estilos condicionales
 
 const PadreRutas = () => {
   return (
@@ -11,16 +13,12 @@ const PadreRutas = () => {
     <div id="" className="default-card">
 
         <BrowserRouter>
-            <table className='navbar-table'>
-                <tbody>
-                <tr>
-                    <td><NavLink className={({isActive}) => isActive ? "estiloLink": ""} exact="true" to="/"><i className="bi bi-house-door"></i></NavLink></td>
-                    <td><Link className={({isActive}) => isActive ? "estiloLink": ""} to="/pagina2">2</Link></td>
-                    <td><NavLink className={({isActive}) => isActive ? "estiloLink": ""} to="/pagina3">3</NavLink></td>
-                    <td><NavLink className={({isActive}) => isActive ? "estiloLink": ""} to="/pagina4">4</NavLink></td>
-                </tr>
-                </tbody>           
-            </table>
+            <ul className='navbar-list'>
+                    <li><NavLink className={({isActive}) => isActive ? "estiloLink": ""} exact="true" to="/"><i className="bi bi-house-door"></i></NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? "estiloLink": ""} to="/pagina2">2</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? "estiloLink": ""} to="/pagina3">3</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? "estiloLink": ""} to="/pagina4">4</NavLink></li>
+            </ul>
             <Routes>
             
                 <Route path="/" exact Component={Componente1}/>
@@ -32,8 +30,8 @@ const PadreRutas = () => {
                     </>
                     }>
                 </Route>
+                <Route path='/users/:id' Component={RutasDinamicas}/>
             </Routes>    
-               
         </BrowserRouter>
     
         
---
+++ react-udemy/src/Components/RutasDinamicas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RutasDinamicas = (props) => {
+    console.log(props)
+    return (
+    <>
+    Esta es una ruta Dinamica
+    </>
+  )
+}
+
+export default RutasDinamicas
