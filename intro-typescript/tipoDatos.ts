8ddfce87976f3c310697b41252b9c2c25448ac75
Felix Delgadillo <felix.d.barrios@gmail.com>
2024-10-29 17:56:20 -0600

    Se empieza typescript

---
+++ intro-typescript/tipoDatos.ts
@@ -0,0 +1,22 @@
+
+//boolean
+let esHombre:boolean=true
+
+console.log(typeof esHombre)
+
+//numbers (enteros y decimales)
+let numero:number=35
+numero=4.8
+
+//strings
+let frase:string="Hola mundo"
+
+//Any nani?
+let otrogato:any="Quien anda ahi"
+otrogato=1
+otrogato="gato"
+otrogato=false
+
+// tuplas y arrays
+let lista:string[]=['pan', 'queso', 'tomate']
+let tup:[string, number, boolean]=['pizza', 33.95, true]
