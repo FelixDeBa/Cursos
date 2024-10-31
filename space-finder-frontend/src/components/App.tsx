d1421285fd8971ad8a1e824c5cccc8a10e87ac9f
Felix Delgadillo <felix.d.barrios@gmail.com>
2024-10-31 17:57:02 -0600

    Se empiezan componentes clase Typescript

---
+++ space-finder-frontend/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from '../logo.svg';
+import './App.css';
+
+export class App extends React.Component {
+  render(){
+    return(
+    <div className="">App is working after making it a class</div>
+    );
+
+  }
+}
+
+export default App;
