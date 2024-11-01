faac1786395dc4bc2aff2a55822f247bbcf422b7
Felix Delgadillo <felix.d.barrios@gmail.com>
2024-11-01 17:54:54 -0600

    Se agrega correccion al boton de login

--- typescript-react/src/components/Navbar.tsx
+++ typescript-react/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { User } from '../model/Model'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import style from './css/Navbar.module.css'
 import { driver } from 'driver.js'
 import 'driver.js/dist/driver.css'
@@ -18,17 +18,17 @@ export class Navbar extends React.Component<{user:User | undefined}>{
     render(){
         let loginLogout:any
         if(this.props.user){
-            loginLogout =<Link to='/logout'>{this.props.user.userName}</Link>
+            loginLogout =<Link to='/logout' style={{float:'right'}}>{this.props.user.userName}</Link>
         }else{
-            loginLogout =<Link to='/login'>Login</Link>
+            loginLogout =<Link to='/login' style={{float:'right'}}>Login</Link>
         }
 
         return(
             <div className="navbar">
                 <Link id="home" to="/">Home</Link>
                 <Link id="profile" to="/profile">Profile</Link>
-                <Link id="login" to="/login">Login</Link>
-                <button onClick={e=> driverObj.drive()}>Tour</button>
+                {loginLogout}
+                <button style={{float:'right'}} onClick={e=> driverObj.drive()}>Tour</button>
             </div>
         )
     }
