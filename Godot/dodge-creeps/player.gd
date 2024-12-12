b26f3a0111c68e2315d0764852cb90a9afb8dcd2
Felix Delgadillo <felix.d.barrios@gmail.com>
2024-12-12 12:51:57 -0600

    Se termina el tutorial de juego basico de Godot

--- Godot/dodge-creeps/player.gd
+++ Godot/dodge-creeps/player.gd
@@ -1,10 +1,14 @@
 extends Area2D
+signal hit
 @export var speed = 400
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	
+	#Evita que se vea el jugador cuando inicia la pantalla
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -26,3 +30,31 @@ func _process(delta: float) -> void:
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	#start(position)
+	
+	
+	#Hace las rotaciones en horizontal y vertical de las animaciones
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		#$AnimatedSprite2D.rotation = deg_to_rad(270) if velocity.x > 0 else deg_to_rad(90)
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		#$AnimatedSprite2D.rotation = deg_to_rad(180) if velocity.x > 0 else deg_to_rad(0)
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	#else:
+		#$AnimatedSprite2D.rotation = deg_to_rad(0)
+		
+
+#Esta funcion se ejecuta siempre que hay una colision de un objeto con el jugador
+func _on_body_entered(body: Node2D) -> void:
+	#Borra al jugador de la escena cuando es golpeado
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
