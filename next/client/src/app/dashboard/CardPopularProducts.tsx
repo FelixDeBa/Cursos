65c6ecdbae811f7a3993a39a566046776c661a34
Felix Delgadillo <felix.d.barrios@gmail.com>
2025-01-15 17:57:29 -0600

    Se agregan el dashboard quedan bugs

--- next/client/src/app/dashboard/CardPopularProducts.tsx
+++ next/client/src/app/dashboard/CardPopularProducts.tsx
@@ -1,5 +1,7 @@
 import { useGetDashboardMetricsQuery } from '@/state/api';
+import { ShoppingBag } from 'lucide-react';
 import React from 'react'
+import Rating from '../(components)/Rating';
 
 const CardPopularProducts = () => {
     const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
@@ -24,9 +26,16 @@ const CardPopularProducts = () => {
                                         <div className="flex text-sm items-center">
                                             <span className="font-bold text-blue-500 text-xs">${product.price}</span>
                                             <span className="mx-2">|</span>
-                                            <div>rating</div>
+                                            <Rating rating={product.rating || 0} />
                                         </div>
                                     </div>
+                                </div>
+
+                                <div className="text-xs flex items-center">
+                                    <button className="p-2 rounded-full bg-blue-100 text-blue-600 mr-2">
+                                        <ShoppingBag className="w-4 h-4"/>
+                                    </button>
+                                    {Math.round(product.stockQuantity / 1000)}k Sold
                                 </div>
                             </div>
                         ))}
--- next/server/src/controllers/dashboardController.ts
+++ next/server/src/controllers/dashboardController.ts
@@ -15,7 +15,7 @@ export const getDashboardMetrics = async (
             },
         });
 
-        const saleSummary = await prisma.salesSummary.findMany({
+        const salesSummary = await prisma.salesSummary.findMany({
             take:5,
             orderBy:{
                 date: "desc",
@@ -52,7 +52,7 @@ export const getDashboardMetrics = async (
 
         res.json({
             popularProducts,
-            saleSummary,
+            salesSummary,
             purchaseSummary,
             expenseSummary,
             expenseByCategorySummary
